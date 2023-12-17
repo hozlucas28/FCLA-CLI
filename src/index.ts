54b50758b7a3a1657f26b08e7c09e1667b00ba6b
hozlucas28 <hozlucas28@gmail.com>
2023-12-17 00:35:48 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 //#!/usr/bin/env node
+import { end, intro } from './outputs'
+import { createSettings } from './pre-init'
+import { factoryRestore } from './factoryRestore'
+import { isFirstInit } from './isFirstInit'
 
-function main() {
-	console.log('Hello World!')
+async function main() {
+	if (process.env.FACTORY_RESTORE) await factoryRestore()
+
+	await intro()
+	const firstInit = await isFirstInit()
+
+	if (firstInit) {
+		await createSettings({})
+		await end()
+		return
+	}
 }
 
 main()
