e7d39507bd9407f76d18daca95934f081e555d9a
hozlucas28 <hozlucas28@gmail.com>
2023-12-29 18:07:04 -0300

    Update string message

--- src/inputs/getRespawnDelay.input.ts
+++ src/inputs/getRespawnDelay.input.ts
@@ -8,7 +8,7 @@ export async function getRespawnDelay(): Promise<number | undefined> {
 	const { delay }: Prompts<'delay', number> = await prompts({
 		type: 'number',
 		name: 'delay',
-		message: 'Tiempo de espera para respawnear (en segundos)',
+		message: 'Tiempo de espera para reaparecer (en segundos)',
 		initial: 120,
 		min: 0,
 		max: 300,
