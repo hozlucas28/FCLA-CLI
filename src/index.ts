0ec58a52cecda23a472c8c7679b1f2bfad662df0
hozlucas28 <hozlucas28@gmail.com>
2024-01-11 21:27:36 -0300

    Update index structure

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,27 @@
 //#!/usr/bin/env node
-import { operationActions } from './actions'
 import { factoryRestore } from './factoryRestore'
-import { getOperation } from './inputs'
 import { isFirstInit } from './isFirstInit'
-import { isValidDirectory } from './isValidDirectory'
+import { isValidDir } from './isValidDir'
 import { end, intro, text } from './outputs'
 import { createSettings } from './pre-init'
 
 async function main() {
-	if (process.env.FACTORY_RESTORE === 'true') await factoryRestore()
-
+	if (process.env.FACTORY_RESTORE) await factoryRestore()
 	await intro()
-	const firstInit = await isFirstInit()
 
-	if (firstInit) {
+	if (await isFirstInit()) {
 		await createSettings({})
 		await end()
-		return
 	}
 
-	const validDirectory = await isValidDirectory()
-	if (!validDirectory) {
+	if (!(await isValidDir())) {
 		await text({
 			color: 'red',
 			message:
 				'¡Directorio de trabajo inválido!\n\nEjecute el CLI en un directorio\ndonde se encuentre un archivo: mission.sqm.',
 		})
 		await end()
-		return
 	}
-
-	const operation = await getOperation()
-	if (!operation) {
-		await end()
-		return
-	}
-
-	await operationActions[operation]()
-	await end()
 }
 
 main()
