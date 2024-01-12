9ea607bf48eed4bd302d50adf9dae4463ed90214
hozlucas28 <hozlucas28@gmail.com>
2024-01-11 21:17:48 -0300

    Add exit code param

--- src/outputs/end.output.ts
+++ src/outputs/end.output.ts
@@ -1,5 +1,6 @@
 import { text } from './text.output'
 
-export async function end(): Promise<void> {
+export async function end(code: 0 | 1): Promise<void> {
 	await text({ message: 'CLI finalizado', color: 'gray' })
+	process.exit(code)
 }
