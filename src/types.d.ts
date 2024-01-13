12a8187aebcc7606e8245e9f52637840cca1cd35
hozlucas28 <hozlucas28@gmail.com>
2024-01-12 23:16:52 -0300

    Update Operation type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
-export type Operation = 'configure-scenario' | 'restore-settings'
+export type Operation = 'configure-scenario' | 'restore-factory-settings'
 
 export type Settings = {
 	author: string
