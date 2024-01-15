c4c8efa0a1a01958cdebbed2af9f5e79a42e2397
hozlucas28 <hozlucas28@gmail.com>
2024-01-15 16:21:38 -0300

    Improve code

--- src/inputs/getOperation.input.ts
+++ src/inputs/getOperation.input.ts
@@ -6,21 +6,23 @@ import type { Choices, Prompts } from './types'
 export async function getOperation(): Promise<Operation | undefined> {
 	await lineBreak()
 
+	const choices: Choices<Operation> = [
+		{
+			title: 'Configurar el escenario',
+			value: 'configure-scenario',
+			selected: true,
+		},
+		{
+			title: 'Restaurar valores de fábrica',
+			value: 'restore-factory-settings',
+		},
+	]
+
 	const { operation }: Prompts<'operation', Operation> = await prompts({
 		type: 'select',
 		name: 'operation',
 		message: 'Seleccione la operación a ejecutar:',
-		choices: [
-			{
-				title: 'Configurar el escenario',
-				value: 'configure-scenario',
-				selected: true,
-			},
-			{
-				title: 'Restaurar valores de fábrica',
-				value: 'restore-factory-settings',
-			},
-		] as Choices<Operation>,
+		choices,
 	})
 
 	return operation
