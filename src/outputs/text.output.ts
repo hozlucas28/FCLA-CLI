af7eab51e3399f7a24aa3cbef400de34aa1398e3
hozlucas28 <hozlucas28@gmail.com>
2024-01-18 12:19:36 -0300

    Refactor text output function to support an optional icon parameter

--- src/outputs/text.output.ts
+++ src/outputs/text.output.ts
@@ -1,11 +1,19 @@
 import pico from 'picocolors'
 
 type Params = {
+	icon?: string
 	message: string
 	color: 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray'
 }
 
-export async function text({ message, color }: Params): Promise<void> {
-	const styledMessage = pico[color](message)
+export async function text({ icon = '', message, color }: Params): Promise<void> {
+	const iconFmt = icon === '' ? '' : `${icon} `
+	const messageFmt = message
+		.split('\n')
+		.map((line, index) => (index === 0 ? line : line.padStart(iconFmt.length + line.length)))
+		.join('\n')
+
+	const styledMessage = pico[color](`${iconFmt}${messageFmt}`)
+
 	console.log(`\n${styledMessage}`)
 }
